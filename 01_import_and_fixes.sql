f684a01c2cfae97a69a3f5f4f8e6febd5b91b930
KamilDataAnalyst <gkamil0706@gmail.com>
2026-06-27 08:03:49 +0200

    Skrócenie komentarzy

--- 01_import_and_fixes.sql
+++ 01_import_and_fixes.sql
@@ -6,6 +6,6 @@ ALTER COLUMN kwota FLOAT;
 ALTER TABLE TabelaTransakcje
 ALTER COLUMN data_transakcji DATETIME2;
 
--- 3. Standaryzacja klucza obcego (konto_id) na INT
+-- 3. Standaryzacja klucza obcego (konto_id) na SMALLINT
 ALTER TABLE TabelaTransakcje
-ALTER COLUMN konto_id INT;
+ALTER COLUMN konto_id SMALLINT;
